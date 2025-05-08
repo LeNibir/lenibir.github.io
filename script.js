271994b0dee8ea3262a73d894edcb64205d186e6
LeNibir <pumpkinlover237@gmail.com>
2025-05-08 22:44:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function  Itoldyoutonotclickmehahah() {
+document.getElementById("message").innerText ="BRUH I TOLD YOU TO NOT CLICK";
+let colors = ["#f0f0f0", "#ffe4e1", "#e6e6fa", "#f5f5dc", "#d0f0c0"];
+let randomcolors = colors[Math.floor(Math.random() * colors.length)]
+document.body.style.backgroundColor = randomcolors ;
+
+}
